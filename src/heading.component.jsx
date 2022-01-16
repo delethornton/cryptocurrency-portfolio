6e2c147e36cbe25ce309c7171b3547d747834741
Derrick <delthornton@pm.me>
2022-01-16 15:12:38 -0600

    Updated the README, removed crawling, changed the metadata inside of the manifest.json, removed unnecessary boilerplate code from App.css/.js and created structural components: Header, Footer, and PricingArea. Also, included heading and searchbar in Header.

---
+++ src/heading.component.jsx
@@ -0,0 +1,4 @@
+const Heading = () => {
+  return <h1>Crypto Price Checker</h1>;
+};
+export default Heading;
---
+++ src/searchBar.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <input type="search"></input>;
+  }
+}
+
+export default SearchBar;
